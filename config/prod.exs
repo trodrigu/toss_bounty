a0d2e3e1eee09f3eef8538f0b9da61d6b15d8ad4
Tommy Rodriguez <trodriguez91@icloud.com>
2017-12-03 02:54:55 -0800

    set oauth2 env vars in prod

--- config/prod.exs
+++ config/prod.exs
@@ -70,3 +70,8 @@ import_config "prod.secret.exs"
 config :toss_bounty, repo_grabber: TossBounty.SellableRepos.TentacatReposGrabber
 config :toss_bounty, issue_grabber: TossBounty.SellableIssues.TentacatIssuesGrabber
 config :toss_bounty, github_strategy: TossBounty.GithubStrategy
+
+config :oauth2, GitHub,
+  client_id: System.get_env("GITHUB_CLIENT_ID"),
+  client_secret: System.get_env("GITHUB_CLIENT_SECRET"),
+  redirect_uri: System.get_env("GITHUB_REDIRECT_URI")
