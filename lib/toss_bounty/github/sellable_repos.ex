a58b42ba4cef4f6db50bd4f36a2f989eec4c140c
trodrigu <trodriguez91@icloud.com>
2017-12-16 20:30:25 -0800

    convert to sort based on stargazers

--- lib/toss_bounty/github/sellable_repos.ex
+++ lib/toss_bounty/github/sellable_repos.ex
@@ -6,8 +6,38 @@ defmodule TossBountyWeb.SellableRepos do
   end
 
   def call(user) do
-    @sellable_repo_impl.list_mine(user)
-    |> Enum.filter(fn(x) -> x["open_issues_count"] > 0 end)
-    |> Enum.map(fn(repo) -> %{ name: repo["name"], owner: repo["owner"]["login"] } end)
+    sellable_repos =
+      @sellable_repo_impl.list_mine(user)
+      |> Enum.filter(fn(x) -> x["stargazers_count"] > 0 end)
+      |> Enum.map(fn(repo) -> %{ name: repo["name"], owner: repo["owner"]["login"], bountiful_score: calculate_bountiful_score(repo) } end)
+  end
+
+  defp calculate_bountiful_score(repo) do
+    stargazers_count = repo["stargazers_count"]
+    look_up_tier(stargazers_count)
+  end
+
+  defp look_up_tier(stargazers_count) do
+    index =
+      tiers
+      |> Enum.find_index(fn (tier) -> check_tier(tier, stargazers_count) end)
+    return_index(index)
+  end
+
+  defp check_tier(tier, stargazers_count) do
+    Enum.member?( tier, stargazers_count )
+  end
+
+  defp return_index(nil), do: 5
+  defp return_index(index), do: index
+
+  defp tiers do
+    [
+      1..20,
+      20..40,
+      40..60,
+      60..80,
+      80..100,
+    ]
   end
 end
--- test/toss_bounty/github/sellable_repos_test.exs
+++ test/toss_bounty/github/sellable_repos_test.exs
@@ -7,54 +7,94 @@ defmodule TossBountyWeb.SellableReposTest do
 
   describe "call/2" do
 
-    test "when there are zero repos with issues" do
+    test "when there are zero repos with stargazers" do
       user = with {:ok, user} <- Repo.insert!(%User{email: "trodriguez91@icloud.com"}), do: user
       SellableRepos.MockReposGrabber.clear
-      SellableRepos.MockReposGrabber.insert_repo(%{ "name" => "Barter", "open_issues_count" => 0, "owner" => %{ "login" => "smcfarlane" } })
+      SellableRepos.MockReposGrabber.insert_repo(%{ "name" => "Barter", "stargazers_count" => 0, "owner" => %{ "login" => "smcfarlane" } })
       result = SellableRepos.call(user)
       assert Enum.count(result) == 0
     end
 
-    test "when there is one repo with more than zero issues" do
+    test "when there is one repo with more than zero stargazers" do
       SellableRepos.MockReposGrabber.clear
       user = with {:ok, user} <- Repo.insert!(%User{email: "test@example.com"}), do: user
-      SellableRepos.MockReposGrabber.insert_repo(%{ "name" => "toss_bounty", "open_issues_count" => 3, "owner" => %{ "login" => "smcfarlane" } })
+      SellableRepos.MockReposGrabber.insert_repo(%{ "name" => "toss_bounty", "stargazers_count" => 3, "owner" => %{ "login" => "smcfarlane" } })
       result = SellableRepos.call(user)
       assert Enum.count(result) == 1
-      assert [%{ name: "toss_bounty", owner: "smcfarlane" }] == result
+      assert [%{ name: "toss_bounty", owner: "smcfarlane", bountiful_score: 0 }] == result
     end
 
-    test "when there are ten repos with more than zero issues" do
+    test "when there are ten repos with more than zero stargazers" do
       SellableRepos.MockReposGrabber.clear
       user = with {:ok, user} <- Repo.insert!(%User{email: "test@example.com"}), do: user
       repos =
         [
-          %{ "name" => "awesome_repo_1", "open_issues_count" => 3, "owner" => %{ "login" => "smcfarlane" } },
-          %{ "name" => "awesome_repo_2", "open_issues_count" => 3, "owner" => %{ "login" => "smcfarlane" } },
-          %{ "name" => "awesome_repo_3", "open_issues_count" => 3, "owner" => %{ "login" => "smcfarlane" } },
-          %{ "name" => "awesome_repo_4", "open_issues_count" => 3, "owner" => %{ "login" => "smcfarlane" } },
-          %{ "name" => "awesome_repo_5", "open_issues_count" => 3, "owner" => %{ "login" => "smcfarlane" } },
-          %{ "name" => "awesome_repo_6", "open_issues_count" => 3, "owner" => %{ "login" => "smcfarlane" } },
-          %{ "name" => "awesome_repo_7", "open_issues_count" => 3, "owner" => %{ "login" => "smcfarlane" } },
-          %{ "name" => "awesome_repo_8", "open_issues_count" => 3, "owner" => %{ "login" => "smcfarlane" } },
-          %{ "name" => "awesome_repo_9", "open_issues_count" => 3, "owner" => %{ "login" => "smcfarlane" } },
-          %{ "name" => "awesome_repo_10", "open_issues_count" => 3, "owner" => %{ "login" => "smcfarlane" } },
+          %{ "name" => "awesome_repo_1", "stargazers_count" => 3, "owner" => %{ "login" => "smcfarlane" } },
+          %{ "name" => "awesome_repo_2", "stargazers_count" => 3, "owner" => %{ "login" => "smcfarlane" } },
+          %{ "name" => "awesome_repo_3", "stargazers_count" => 3, "owner" => %{ "login" => "smcfarlane" } },
+          %{ "name" => "awesome_repo_4", "stargazers_count" => 3, "owner" => %{ "login" => "smcfarlane" } },
+          %{ "name" => "awesome_repo_5", "stargazers_count" => 3, "owner" => %{ "login" => "smcfarlane" } },
+          %{ "name" => "awesome_repo_6", "stargazers_count" => 3, "owner" => %{ "login" => "smcfarlane" } },
+          %{ "name" => "awesome_repo_7", "stargazers_count" => 3, "owner" => %{ "login" => "smcfarlane" } },
+          %{ "name" => "awesome_repo_8", "stargazers_count" => 3, "owner" => %{ "login" => "smcfarlane" } },
+          %{ "name" => "awesome_repo_9", "stargazers_count" => 3, "owner" => %{ "login" => "smcfarlane" } },
+          %{ "name" => "awesome_repo_10", "stargazers_count" => 3, "owner" => %{ "login" => "smcfarlane" } },
         ]
       SellableRepos.MockReposGrabber.insert_repos repos
       result = SellableRepos.call(user)
       assert Enum.count(result) == 10
       assert [
-        %{ name: "awesome_repo_1", owner: "smcfarlane" },
-        %{ name: "awesome_repo_2", owner: "smcfarlane" },
-        %{ name: "awesome_repo_3", owner: "smcfarlane" },
-        %{ name: "awesome_repo_4", owner: "smcfarlane" },
-        %{ name: "awesome_repo_5", owner: "smcfarlane" },
-        %{ name: "awesome_repo_6", owner: "smcfarlane" },
-        %{ name: "awesome_repo_7", owner: "smcfarlane" },
-        %{ name: "awesome_repo_8", owner: "smcfarlane" },
-        %{ name: "awesome_repo_9", owner: "smcfarlane" },
-        %{ name: "awesome_repo_10", owner: "smcfarlane" }
+        %{ name: "awesome_repo_1", owner: "smcfarlane", bountiful_score: 0 },
+        %{ name: "awesome_repo_2", owner: "smcfarlane", bountiful_score: 0 },
+        %{ name: "awesome_repo_3", owner: "smcfarlane", bountiful_score: 0 },
+        %{ name: "awesome_repo_4", owner: "smcfarlane", bountiful_score: 0 },
+        %{ name: "awesome_repo_5", owner: "smcfarlane", bountiful_score: 0 },
+        %{ name: "awesome_repo_6", owner: "smcfarlane", bountiful_score: 0 },
+        %{ name: "awesome_repo_7", owner: "smcfarlane", bountiful_score: 0 },
+        %{ name: "awesome_repo_8", owner: "smcfarlane", bountiful_score: 0 },
+        %{ name: "awesome_repo_9", owner: "smcfarlane", bountiful_score: 0 },
+        %{ name: "awesome_repo_10", owner: "smcfarlane", bountiful_score: 0 }
       ] == result
+    end
+
+    test "when there is one repo with between 20 to 40 stargazers returns score of 1" do
+      SellableRepos.MockReposGrabber.clear
+      user = with {:ok, user} <- Repo.insert!(%User{email: "test@example.com"}), do: user
+      SellableRepos.MockReposGrabber.insert_repo(%{ "name" => "toss_bounty", "stargazers_count" => 21, "owner" => %{ "login" => "smcfarlane" } })
+      result = SellableRepos.call(user)
+      assert [%{ name: "toss_bounty", owner: "smcfarlane", bountiful_score: 1 }] == result
+    end
+
+    test "when there is one repo with between 40 to 60 stargazers returns score of 2" do
+      SellableRepos.MockReposGrabber.clear
+      user = with {:ok, user} <- Repo.insert!(%User{email: "test@example.com"}), do: user
+      SellableRepos.MockReposGrabber.insert_repo(%{ "name" => "toss_bounty", "stargazers_count" => 41, "owner" => %{ "login" => "smcfarlane" } })
+      result = SellableRepos.call(user)
+      assert [%{ name: "toss_bounty", owner: "smcfarlane", bountiful_score: 2 }] == result
+    end
+
+    test "when there is one repo with between 60 to 80 stargazers returns score of 3" do
+      SellableRepos.MockReposGrabber.clear
+      user = with {:ok, user} <- Repo.insert!(%User{email: "test@example.com"}), do: user
+      SellableRepos.MockReposGrabber.insert_repo(%{ "name" => "toss_bounty", "stargazers_count" => 61, "owner" => %{ "login" => "smcfarlane" } })
+      result = SellableRepos.call(user)
+      assert [%{ name: "toss_bounty", owner: "smcfarlane", bountiful_score: 3 }] == result
+    end
+
+    test "when there is one repo with between 80 to 100 stargazers returns score of 4" do
+      SellableRepos.MockReposGrabber.clear
+      user = with {:ok, user} <- Repo.insert!(%User{email: "test@example.com"}), do: user
+      SellableRepos.MockReposGrabber.insert_repo(%{ "name" => "toss_bounty", "stargazers_count" => 81, "owner" => %{ "login" => "smcfarlane" } })
+      result = SellableRepos.call(user)
+      assert [%{ name: "toss_bounty", owner: "smcfarlane", bountiful_score: 4 }] == result
+    end
+
+    test "when there is one repo with over 100 stargazers returns score of 5" do
+      SellableRepos.MockReposGrabber.clear
+      user = with {:ok, user} <- Repo.insert!(%User{email: "test@example.com"}), do: user
+      SellableRepos.MockReposGrabber.insert_repo(%{ "name" => "toss_bounty", "stargazers_count" => 101, "owner" => %{ "login" => "smcfarlane" } })
+      result = SellableRepos.call(user)
+      assert [%{ name: "toss_bounty", owner: "smcfarlane", bountiful_score: 5 }] == result
     end
   end
 end
