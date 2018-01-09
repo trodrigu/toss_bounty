5275f8487cce0f8fed28ed976fe2a605356eba85
trodrigu <trodriguez91@icloud.com>
2018-01-09 11:52:31 -0800

    rename to campaigns test

--- test/toss_bounty/campaigns/campaigns_test.exs
+++ test/toss_bounty/campaigns/campaigns_test.exs
@@ -3,10 +3,12 @@ defmodule TossBounty.CampaignsTest do
 
   alias TossBounty.Campaigns
   alias TossBounty.Accounts.User
+  alias TossBounty.GitHub.GitHubRepo
 
   setup do
     user = with {:ok, user} <- Repo.insert!(%User{email: "test@test.com"}), do: user
-    {:ok, user: user}
+    github_repo = with {:ok, github_repo} <- Repo.insert!(%GitHubRepo{name: "elm-lang", owner: "trodrigu", bountiful_score: 5, user_id: user.id}), do: github_repo
+    {:ok, user: user, github_repo: github_repo}
   end
 
   describe "campaigns" do
@@ -36,22 +38,25 @@ defmodule TossBounty.CampaignsTest do
 
     def campaign_fixture(attrs \\ %{}) do
       user = attrs[:user]
-      attrs = Map.put(@valid_attrs, :user_id, user.id)
+      github_repo = attrs[:github_repo]
+      attrs_with_user_id = Map.put(@valid_attrs, :user_id, user.id)
+      attrs_with_github_repo = Map.put(@valid_attrs, :github_repo_id, github_repo.id)
       {:ok, campaign} =
-        attrs
+        attrs_with_user_id
+        |> Enum.into(attrs_with_github_repo)
         |> Enum.into(@valid_attrs)
         |> Campaigns.create_campaign()
 
       campaign
     end
 
-    test "list_campaigns/0 returns all campaigns", %{user: user} do
-      campaign = campaign_fixture(%{user: user})
+    test "list_campaigns/0 returns all campaigns", %{user: user, github_repo: github_repo} do
+      campaign = campaign_fixture(%{user: user, github_repo: github_repo})
       assert Campaigns.list_campaigns() == [campaign]
     end
 
-    test "get_campaign!/1 returns the campaign with given id", %{user: user} do
-      campaign = campaign_fixture(%{user: user})
+    test "get_campaign!/1 returns the campaign with given id", %{user: user, github_repo: github_repo} do
+      campaign = campaign_fixture(%{user: user, github_repo: github_repo})
       assert Campaigns.get_campaign!(campaign.id) == campaign
     end
 
@@ -69,8 +74,8 @@ defmodule TossBounty.CampaignsTest do
       assert {:error, %Ecto.Changeset{}} = Campaigns.create_campaign(@invalid_attrs)
     end
 
-    test "update_campaign/2 with valid data updates the campaign", %{user: user} do
-      campaign = campaign_fixture(%{user: user})
+    test "update_campaign/2 with valid data updates the campaign", %{user: user, github_repo: github_repo} do
+      campaign = campaign_fixture(%{user: user, github_repo: github_repo})
       assert {:ok, campaign} = Campaigns.update_campaign(campaign, @update_attrs)
       assert %Campaign{} = campaign
       assert campaign.current_funding == 456.7
@@ -80,20 +85,20 @@ defmodule TossBounty.CampaignsTest do
       assert campaign.short_description == "some updated short_description"
     end
 
-    test "update_campaign/2 with invalid data returns error changeset", %{user: user} do
-      campaign = campaign_fixture(%{user: user})
+    test "update_campaign/2 with invalid data returns error changeset", %{user: user, github_repo: github_repo} do
+      campaign = campaign_fixture(%{user: user, github_repo: github_repo})
       assert {:error, %Ecto.Changeset{}} = Campaigns.update_campaign(campaign, @invalid_attrs)
       assert campaign == Campaigns.get_campaign!(campaign.id)
     end
 
-    test "delete_campaign/1 deletes the campaign", %{user: user} do
-      campaign = campaign_fixture(%{user: user})
+    test "delete_campaign/1 deletes the campaign", %{user: user, github_repo: github_repo} do
+      campaign = campaign_fixture(%{user: user, github_repo: github_repo})
       assert {:ok, %Campaign{}} = Campaigns.delete_campaign(campaign)
       assert_raise Ecto.NoResultsError, fn -> Campaigns.get_campaign!(campaign.id) end
     end
 
-    test "change_campaign/1 returns a campaign changeset", %{user: user}  do
-      campaign = campaign_fixture(%{user: user})
+    test "change_campaign/1 returns a campaign changeset", %{user: user, github_repo: github_repo}  do
+      campaign = campaign_fixture(%{user: user, github_repo: github_repo})
       assert %Ecto.Changeset{} = Campaigns.change_campaign(campaign)
     end
   end
