d885735aaf87f50fa26482c018ec2f357df02887
trodrigu <trodriguez91@icloud.com>
2018-01-20 14:50:40 -0800

    create policy for reward update

---
+++ lib/toss_bounty/policy/reward.ex
@@ -0,0 +1,7 @@
+defmodule TossBounty.Policy.Reward do
+
+  alias TossBounty.{Incentive.Reward, Accounts.User}
+  import TossBounty.Policy.Helpers, only: [belongs_to?: 2]
+
+  def update?(%User{} = user, %Reward{} = reward), do: reward |> belongs_to?(user)
+end
---
+++ test/toss_bounty/policy/reward_test.exs
@@ -0,0 +1,57 @@
+defmodule TossBounty.Policy.RewardTest do
+  use TossBounty.PolicyCase
+
+  import TossBounty.Policy.Reward, only: [update?: 2]
+
+  alias TossBounty.Incentive.Reward
+  alias TossBounty.Incentive
+  alias TossBounty.GitHub.GithubRepo
+  alias TossBounty.Accounts.User
+  alias TossBounty.Repo
+  alias TossBounty.Campaigns
+
+  @reward_attrs %{
+    description: "some description",
+    donation_level: 120.5,
+  }
+
+  @campaign_attrs %{
+    current_funding: 120.5,
+    funding_end_date: Timex.parse!("Tue, 06 Mar 2013 01:25:19 +0200", "{RFC1123}"),
+    funding_goal: 120.5,
+    long_description: "some long_description",
+    short_description: "some short_description",
+  }
+
+  setup do
+    user = Repo.insert!(%User{email: "test@test.com"})
+    github_repo = Repo.insert!(%GithubRepo{ user_id: user.id })
+    attrs_with_user = Map.put(@campaign_attrs, :user_id, user.id)
+    attrs = Map.put(attrs_with_user, :github_repo_id, github_repo.id)
+    {:ok, campaign} =
+      Campaigns.create_campaign(attrs)
+    {:ok, user: user, campaign: campaign}
+  end
+
+  describe "update?" do
+    test "returns true when reward belongs to a user", %{user: user, campaign: campaign} do
+      attrs = Map.put(@reward_attrs, :campaign_id, campaign.id)
+
+      {:ok, reward} =
+        Incentive.create_reward(attrs)
+
+      assert update?(user, reward)
+    end
+
+    test "returns false when reward belongs to a user", %{user: user, campaign: campaign} do
+      attrs = Map.put(@reward_attrs, :campaign_id, campaign.id)
+
+      {:ok, reward} =
+        Incentive.create_reward(attrs)
+
+      another_user = Repo.insert!(%User{email: "another_test@test.com"})
+
+      refute update?(another_user, reward)
+    end
+  end
+end
