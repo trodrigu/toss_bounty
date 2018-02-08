298dd4aa8a3db5cd6a9e4a7a901c050fab38d55d
trodrigu <trodriguez91@icloud.com>
2018-02-07 20:31:44 -0800

    make sure to pull in right stripe plan deleter

--- config/dev.exs
+++ config/dev.exs
@@ -50,8 +50,7 @@ config :toss_bounty, issue_grabber: TossBounty.Github.SellableIssues.TentacatIss
 config :toss_bounty, github_strategy: TossBountyWeb.GithubStrategy
 config :toss_bounty, customer_creator: TossBounty.StripeProcessing.StripeCustomerCreator
 config :toss_bounty, plan_creator: TossBounty.StripeProcessing.StripePlanCreator
-config :toss_bounty, plan_creator: TossBounty.StripeProcessing.MockStripeSubscriptionCreator
-config :toss_bounty, plan_deleter: TossBounty.StripeProcessing.MockStripeSubscriptionDeleter
+config :toss_bounty, plan_deleter: TossBounty.StripeProcessing.StripePlanDeleter
 config :toss_bounty, subscription_creator: TossBounty.StripeProcessing.StripeSubscriptionCreator
 config :toss_bounty, subscription_deleter: TossBounty.StripeProcessing.StripeSubscriptionDeleter
 
--- lib/toss_bounty/stripe_processing/stripe_plan_deleter.ex
+++ lib/toss_bounty/stripe_processing/stripe_plan_deleter.ex
@@ -1,7 +1,7 @@
 defmodule TossBounty.StripeProcessing.StripePlanDeleter do
   @behaviour TossBountyWeb.PlanController.Behaviour
 
-  def delete(plan_id) do
+  def delete(_conn, plan_id) do
     Stripe.Plan.delete(plan_id)
   end
 end
