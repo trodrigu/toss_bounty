684d2e57472cfa9e157f33c9809b98a41a33da93
trodrigu <trodriguez91@icloud.com>
2018-03-02 19:13:09 -0800

    default for campaign current funding

--- priv/repo/migrations/20171205172721_create_campaign.exs
+++ priv/repo/migrations/20171205172721_create_campaign.exs
@@ -1,23 +1,22 @@
 defmodule TossBounty.Repo.Migrations.CreateCampaigns do
   use Ecto.Migration
 
-
   # Remember to set the add options for each field.
   # ex: primary_key, default, null, size, precision, scale.
   # Also remember to set the value for on_delete and on_update references options
   # ex: :nothing, :delete_all, :nilify_all, :restrict
   def change do
     create table(:campaigns) do
-      add :short_description, :string
-      add :long_description, :binary
-      add :funding_goal, :float
-      add :current_funding, :float
-      add :funding_end_date, :naive_datetime
-      add :user_id, references(:users, on_delete: :nothing)
+      add(:short_description, :string)
+      add(:long_description, :binary)
+      add(:funding_goal, :float)
+      add(:current_funding, :float, default: 0.0)
+      add(:funding_end_date, :naive_datetime)
+      add(:user_id, references(:users, on_delete: :nothing))
 
       timestamps()
     end
 
-    create index(:campaigns, [:user_id])
+    create(index(:campaigns, [:user_id]))
   end
 end
