9fea32c21ad0bb14b1ba7ec29a037eaa4ae1e112
Thomas M Rodriguez <Thomas.M.Rodriguez@kp.org>
2018-04-26 10:28:37 -0700

    make the search campaign centric

--- priv/repo/migrations/20180425210542_toss_bounty_search.exs
+++ priv/repo/migrations/20180425210542_toss_bounty_search.exs
@@ -10,24 +10,33 @@ defmodule TossBounty.Repo.Migrations.TossBountySearch do
 
     execute(
       """
+      CREATE EXTENSION IF NOT EXISTS pg_trgm;
+
+      """
+    )
+
+    execute(
+      """
       CREATE MATERIALIZED VIEW toss_bounty_search AS
       SELECT
-        github_repos.id AS id,
+        campaigns.id AS id,
         github_repos.name AS name,
         (
         setweight(to_tsvector(unaccent(github_repos.name)), 'A') ||
-        setweight(to_tsvector(unaccent(campaigns.long_description)), 'B')
+        setweight(to_tsvector(unaccent(users.name)), 'B')
         ) AS document
-        FROM github_repos
-        LEFT JOIN campaigns
+        FROM campaigns
+        LEFT JOIN github_repos
         ON campaigns.github_repo_id = github_repos.id
-        GROUP BY github_repos.id;
+        LEFT JOIN users
+        ON github_repos.user_id = users.id
+        GROUP BY campaigns.id, github_repos.name, users.name;
       """
     )
 
     create index("toss_bounty_search", ["document"], using: :gin)
 
-    execute("CREATE INDEX toss_bounty_search_github_repo_name_trgm_index ON toss_bounty_search USING gin (title gin_trgm_ops)")
+    execute("CREATE INDEX toss_bounty_search_github_repo_name_trgm_index ON toss_bounty_search USING gin (name gin_trgm_ops)")
 
     create unique_index("toss_bounty_search", [:id])
 
@@ -45,7 +54,7 @@ defmodule TossBounty.Repo.Migrations.TossBountySearch do
 
     execute(
       """
-      CREATE TRIGGER refresh_toss_bounty_search
+      CREATE TRIGGER refresh_toss_bounty_search_for_campaigns
       AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE
       ON campaigns
       FOR EACH STATEMENT
@@ -55,9 +64,19 @@ defmodule TossBounty.Repo.Migrations.TossBountySearch do
 
     execute(
       """
-      CREATE TRIGGER refresh_toss_bounty_search
+      CREATE TRIGGER refresh_toss_bounty_for_github_repos
       AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE
       ON github_repos
+      FOR EACH STATEMENT
+      EXECUTE PROCEDURE refresh_toss_bounty_search();
+      """
+    )
+
+    execute(
+      """
+      CREATE TRIGGER refresh_toss_bounty_for_users
+      AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE
+      ON users
       FOR EACH STATEMENT
       EXECUTE PROCEDURE refresh_toss_bounty_search();
       """
