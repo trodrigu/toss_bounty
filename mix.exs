3df03589af8a442ff64a718ded5aab148299ef55
trodrigu <trodriguez91@icloud.com>
2018-05-07 10:48:12 -0700

    bump version for beta release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TossBounty.Mixfile do
   def project do
     [
       app: :toss_bounty,
-      version: "0.1.2",
+      version: "0.2.0",
       elixir: "~> 1.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
