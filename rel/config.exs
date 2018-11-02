94d7ac6dfd8c2ff5d1a7e66dad9f02a72e9c9eab
trodrigu <trodriguez91@icloud.com>
2018-11-02 12:51:40 -0700

    update mix apps and toml provider path

--- rel/config.exs
+++ rel/config.exs
@@ -39,7 +39,7 @@ environment :prod do
 
   set(
     config_providers: [
-      {Toml.Provider, [path: "${RELEASE_ROOT_DIR}/config.toml"]}
+      {Toml.Provider, [path: "/srv/apps/toss_bounty/config.toml"]}
     ]
   )
 end
